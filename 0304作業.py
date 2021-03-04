97466e842e0fad4a3039287248066093076f1f10
yitai3 <79919343+yitai3@users.noreply.github.com>
2021-03-04 23:14:10 +0800

    Add files via upload

---
+++ 0304作業.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  4 21:42:07 2021
+
+@author: user
+"""
+
+totala = 0 
+
+for a in range(0,50+1,2):
+    totala+=a
+print("偶數總合:",totala)
+
+totalb = 0
+
+for b in range(1,50+1,2):
+    totalb+=b
+print("奇數總合:",totalb)
