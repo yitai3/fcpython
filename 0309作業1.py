849ab244e7d8e737b21bf047ab1a2cc51013baed
yitai3 <79919343+yitai3@users.noreply.github.com>
2021-03-11 18:46:28 +0800

    Add files via upload

---
+++ 0309作業1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range(10,1,-1): #1,2,3,4,5,6,7,8,9
+    for x in range(1,i): #1~1 1~2 ....1~9
+        print(x,end="")
+    print()    
