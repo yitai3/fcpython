a88f4e9fffb1372e526c9ad0dba4b5fd99841e29
yitai3 <79919343+yitai3@users.noreply.github.com>
2021-03-11 21:01:35 +0800

    Update 0309作業1.py

--- 0309作業1.py
+++ 0309作業1.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-for i in range(10,1,-1): #1,2,3,4,5,6,7,8,9
-    for x in range(1,i): #1~1 1~2 ....1~9
+for i in range(10,1,-1): 
+    for x in range(1,i): 
         print(x,end="")
     print()    
