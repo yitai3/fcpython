dec4e73ee4242602b146ca50bab4af2d9b92e590
yitai3 <79919343+yitai3@users.noreply.github.com>
2021-03-12 14:33:26 +0800

    Add files via upload

---
+++ 0309作業2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+        
+for b in range(1,12):
+    if b in range(1,4) :
+        print(" ",end="")
+    if b == 4:
+        print("*")   
+    if b == 6:
+        print("  ***")
+    if b == 7:
+        print(" *****")
+    if b == 8:
+        print("*******")    
+    if b == 9:
+        print(" *****")
+    if b == 10:
+        print("  ***")             
+    if b == 11:
+        print("   *")                
+       
