00ad573d84384f8cc939b30e4e573c30b04614ae
yitai3 <79919343+yitai3@users.noreply.github.com>
2021-03-12 17:43:12 +0800

    Add files via upload

---
+++ 0311作業1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+請隨機取數1~49取6個，且整數不能重複，並放入串列中，印出串列
+"""
+
+import random
+a=random.randint(1,49)
+b=random.randint(1,49)
+c=random.randint(1,49)
+d=random.randint(1,49)
+e=random.randint(1,49)
+f=random.randint(1,49)
+
+s=[a,b,c,d,e,f]
+
+   
+print(s)
+      
+
